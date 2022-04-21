7af4f9bd14cc7ca2a44e7bc823a739ad774ce113
beewp <ganghyoc@gmail.com>
2022-04-21 19:38:34 +0900

    [inut] project

---
+++ aLinkedList/linkedlist.py
@@ -0,0 +1,33 @@
+class LinkedList:
+    def __init__(self, data):
+        self.head = Node(data)
+
+    def append(self, data):
+        cur = self.head
+        while cur.next is not None:
+            cur = cur.next
+        cur.next = Node(data)
+
+    def print_all(self):
+        cur = self.head
+        while cur is not None:
+            print(cur.data)
+            cur = cur.next
+
+    def add_node(self, index, value):
+        new_node = Node(value)
+        if index == 0:
+            new_node.next = self.head
+            self.head = new_node
+            return
+        node = self.get_node(index-1)
+        next_node = node.next
+        node.next = new_node
+        new_node.next = next_node
+
+    def delete_node(self, index):
+        if index == 0:
+            self.head = self.head.next
+            return
+        node = self.get_node(index-1)
+        node.next = node.next.next
